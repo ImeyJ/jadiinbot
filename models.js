36ed79a62e9048096ed7d438ad0e2c186cacad31
FromCelebes <81816756+ImeyJ@users.noreply.github.com>
2022-08-14 12:27:36 +0800

    Update models.js

--- models.js
+++ models.js
@@ -1,60 +1,60 @@
 const { Sequelize, DataTypes } = require('sequelize')
 const sequelize = new Sequelize('sqlite:///' + __dirname + '/db.sqlite3')
 
-// const User = sequelize.define('main_user', {
-//     id: {
-//         type: DataTypes.INTEGER,
-//         allowNull: false,
-//         primaryKey: true,
-//         autoIncrement: true,
-//     },
-//     password: {
-//         type: DataTypes.STRING,
-//         allowNull: false,
-//     },
-//     last_login: {
-//         type: DataTypes.DATE,
-//     },
-//     is_superuser: {
-//         type: DataTypes.BOOLEAN,
-//         allowNull: false,
-//     },
-//     username: {
-//         type: DataTypes.STRING,
-//         allowNull: false,
-//         unique: true,
-//     },
-//     first_name: {
-//         type: DataTypes.STRING,
-//         allowNull: false,
-//     },
-//     last_name: {
-//         type: DataTypes.STRING,
-//         allowNull: false,
-//     },
-//     email: {
-//         type: DataTypes.STRING,
-//         allowNull: false,
-//     },
-//     is_staff: {
-//         type: DataTypes.BOOLEAN,
-//         allowNull: false,
-//     },
-//     is_active: {
-//         type: DataTypes.BOOLEAN,
-//         allowNull: false,
-//     },
-//     date_joined: {
-//         type: DataTypes.DATE,
-//         allowNull: false,
-//     }
+ const User = sequelize.define('main_user', {
+     id: {
+         type: DataTypes.INTEGER,
+         allowNull: false,
+         primaryKey: true,
+         autoIncrement: true,
+     },
+     password: {
+         type: DataTypes.STRING,
+         allowNull: false,
+     },
+     last_login: {
+         type: DataTypes.DATE,
+     },
+     is_superuser: {
+         type: DataTypes.BOOLEAN,
+         allowNull: false,
+     },
+     username: {
+         type: DataTypes.STRING,
+         allowNull: false,
+         unique: true,
+     },
+     first_name: {
+         type: DataTypes.STRING,
+         allowNull: false,
+     },
+     last_name: {
+         type: DataTypes.STRING,
+         allowNull: false,
+     },
+     email: {
+         type: DataTypes.STRING,
+         allowNull: false,
+     },
+     is_staff: {
+         type: DataTypes.BOOLEAN,
+         allowNull: false,
+     },
+     is_active: {
+         type: DataTypes.BOOLEAN,
+         allowNull: false,
+     },
+     date_joined: {
+         type: DataTypes.DATE,
+         allowNull: false,
+     }
 
-// }, {
-//     timestamps: false,
-//     createdAt: false,
-//     updatedAt: false,
-//     freezeTableName: true
-// })
+ }, {
+     timestamps: false,
+     createdAt: false,
+     updatedAt: false,
+     freezeTableName: true
+ })
 
 
 const Session = sequelize.define('main_session', {
@@ -80,9 +80,9 @@ const Session = sequelize.define('main_session', {
     freezeTableName: true
 })
 
-// Session.belongsTo(User, {
-//     foreignKey: 'id',
-// })
+ Session.belongsTo(User, {
+     foreignKey: 'id',
+ })
 
 module.exports = {
     Session
